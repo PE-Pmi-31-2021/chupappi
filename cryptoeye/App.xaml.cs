8759136e3c0c8a4a692a709a47129fd8d04306c3
karrtopelka <karrtopelka@gmail.com>
2021-11-11 00:11:36 +0200

    [ADD] New project with mongo

---
+++ cryptoeye/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace cryptoeye
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private static IConfiguration Configuration { get; set; }
+
+        private static MongoClient Client { get; set; }
+
+        public static IMongoDatabase Db { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+
+            Configuration = builder.Build();
+
+            Client = new MongoClient(Configuration.GetConnectionString("MongoDb"));
+            Db = Client.GetDatabase("cryptoeye");
+        }
+    }
+}
---
+++ cryptoeye/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace cryptoeye
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var user = App.Db.GetCollection<User>("user");
+            user.InsertOne(new User
+            {
+                Username = "karrtopelka", Email = "karrtopelka@gmail.com", Name = "Max", LastName = "Karrtopelka",
+                Password = Hashing.HashPassword("password")
+            });
+        }
+    }
+}
