f0e895a3b817b53103e4f412faa99fd9459ad332
Deains <deinsid@gmail.com>
2021-12-09 00:12:52 +0200

    [ADD] logger

--- cryptoeye/App.xaml.cs
+++ cryptoeye/App.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using log4net;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
+using OxyPlot;
 
 namespace cryptoeye
 {
@@ -16,6 +19,8 @@ namespace cryptoeye
         private static MongoClient Client { get; set; }
         public static IMongoDatabase Db { get; private set; }
 
+        public static readonly ILog Log = LogManager.GetLogger(typeof(App));
+
         protected override void OnStartup(StartupEventArgs e)
         {
             var builder = new ConfigurationBuilder()
@@ -26,6 +31,23 @@ namespace cryptoeye
 
             Client = new MongoClient(Configuration.GetConnectionString("MongoDb"));
             Db = Client.GetDatabase("cryptoeye");
+            log4net.Config.XmlConfigurator.Configure();
+            Log.Info("        =============  Started Logging  =============        ");
+            EventManager.RegisterClassHandler(typeof(TextBox),
+                TextBox.KeyUpEvent,
+                new System.Windows.Input.KeyEventHandler(TextBox_KeyUp));
+            base.OnStartup(e);
+        }
+        private void TextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key != System.Windows.Input.Key.Enter) return;
+            e.Handled = true;
+            Log.Info($"crypto:{sender.ToString().Split(':')[1]}");
+        }
+        
+        public void Plott_MouseDown(object sender, OxyMouseDownEventArgs e)
+        {
+            
         }
     }
 }
--- cryptoeye/MainWindow.xaml.cs
+++ cryptoeye/MainWindow.xaml.cs
@@ -24,101 +24,114 @@ namespace cryptoeye
 
         public MainWindow()
         {
-            InitializeComponent();
-
-            /*var user = App.Db.GetCollection<User>("users");
-            var topics = App.Db.GetCollection<Topic>("topics");
-            var cryptoHistories = App.Db.GetCollection<HistoryPrice>("history_prices");
-            var seenHistories = App.Db.GetCollection<SeenHistory>("seen_histories");*/
-            var cryptos = App.Db.GetCollection<Crypto>("cryptos");
-            var collection = ReadCollection(cryptos);
-            Label.Content = "deinsid@gmail.com";
-
-            var lpg = new LinepointsGenerator();
-            var rng = new Random();
-            for (int i = 1; i < 4; i++)
+            try
             {
-                for (int j = 0; j < 3; j++)
+                InitializeComponent();
+                log4net.Config.XmlConfigurator.Configure();
+
+                /*var user = App.Db.GetCollection<User>("users");
+                var topics = App.Db.GetCollection<Topic>("topics");
+                var cryptoHistories = App.Db.GetCollection<HistoryPrice>("history_prices");
+                var seenHistories = App.Db.GetCollection<SeenHistory>("seen_histories");*/
+                var cryptos = App.Db.GetCollection<Crypto>("cryptos");
+                var collection = ReadCollection(cryptos);
+
+                var lpg = new LinepointsGenerator();
+                var rng = new Random();
+                for (var i = 1; i < 4; i++)
                 {
-                    var border = new Border
+                    for (var j = 0; j < 3; j++)
                     {
-                        CornerRadius = new CornerRadius(15),
-                        Width = 310,
-                        Height = 155,
-                        BorderThickness = new Thickness(1),
-                        Background = new SolidColorBrush(Colors.White)
-                    };
+                        var border = new Border
+                        {
+                            CornerRadius = new CornerRadius(15),
+                            Width = 310,
+                            Height = 155,
+                            BorderThickness = new Thickness(1),
+                            Background = new SolidColorBrush(Colors.White)
+                        };
 
-                    var dynamicLabel1 = new OxyPlot.Wpf.PlotView {Model = new PlotModel()};
+                        var dynamicLabel1 = new OxyPlot.Wpf.PlotView {Model = new PlotModel()};
 
-                    var plotModel = new PlotModel
-                    {
-                        Title = collection[((i-1)*3) + j].Name,
-                        PlotType = PlotType.Cartesian,
-                        Background = OxyColors.White,
-                        TitlePadding = 0,
-                        TitleFontSize = 24
-                    };
-                    var items = lpg.linepointsGenerator(rng.Next(0, 10000));
-                    var max = items.Max(point => point.X);
-                    var min = items.Min(point => point.X);
-                    var timeSPanAxis1 = new DateTimeAxis()
-                    {
-                        Position = AxisPosition.Bottom,
-                        TicklineColor = OxyColor.FromRgb(255,255,255),
-                        IntervalLength = 75,
-                        MinorIntervalType = DateTimeIntervalType.Days,
-                        IntervalType = DateTimeIntervalType.Days,
-                        Maximum = max,
-                        Minimum = min
-                    };
-                    plotModel.Axes.Add(timeSPanAxis1);
-                    max = items.Max(point => point.Y);
-                    min = items.Min(point => point.Y);
-                    LinearAxis linearAxis1 = new LinearAxis()
-                    {
-                        TicklineColor = OxyColor.FromRgb(255,255,255),
-                        Maximum = max,
-                        Minimum = min
-                    };
-                    OxyColor color;
-                    if (items[0].Y < items[items.Length - 1].Y)
-                    {
-                     color = OxyColors.Green;
+                        var plotModel = new PlotModel
+                        {
+                            Title = collection[((i - 1) * 3) + j].Name,
+                            PlotType = PlotType.Cartesian,
+                            Background = OxyColors.White,
+                            TitlePadding = 0,
+                            TitleFontSize = 24
+                        };
+                        var items = lpg.linepointsGenerator(rng.Next(0, 10000));
+                        var max = items.Max(point => point.X);
+                        var min = items.Min(point => point.X);
+                        var timeSPanAxis1 = new DateTimeAxis()
+                        {
+                            Position = AxisPosition.Bottom,
+                            TicklineColor = OxyColor.FromRgb(255, 255, 255),
+                            IntervalLength = 75,
+                            MinorIntervalType = DateTimeIntervalType.Days,
+                            IntervalType = DateTimeIntervalType.Days,
+                            Maximum = max,
+                            Minimum = min
+                        };
+                        plotModel.Axes.Add(timeSPanAxis1);
+                        max = items.Max(point => point.Y);
+                        min = items.Min(point => point.Y);
+                        LinearAxis linearAxis1 = new LinearAxis()
+                        {
+                            TicklineColor = OxyColor.FromRgb(255, 255, 255),
+                            Maximum = max,
+                            Minimum = min
+                        };
+                        OxyColor color;
+                        if (items[0].Y < items[items.Length - 1].Y)
+                        {
+                            color = OxyColors.Green;
 
-                     border.BorderBrush = Brushes.LawnGreen;
+                            border.BorderBrush = Brushes.LawnGreen;
+                        }
+                        else
+                        {
+                            color = OxyColors.Red;
+
+                            border.BorderBrush = Brushes.DarkRed;
+                        }
+
+                        var series = new LineSeries
+                        {
+                            Color = color,
+                            DataFieldX = "X",
+                            ItemsSource = items
+                        };
+
+                        plotModel.Axes.Add(linearAxis1);
+                        plotModel.Series.Add(series);
+                        plotModel.PlotAreaBorderColor = OxyColors.Transparent;
+                        dynamicLabel1.Model = plotModel;
+                        plotModel.MouseDown += (s, e) =>
+                        {
+                            if (e.ChangedButton != OxyMouseButton.Left) return;
+                            e.Handled = true;
+                            App.Log.Info(plotModel.Title);
+                        };
+
+                        dynamicLabel1.Name = "NewLabel";
+                        dynamicLabel1.Width = 300;
+                        dynamicLabel1.Height = 145;
+                        dynamicLabel1.Foreground = new SolidColorBrush(Colors.Black);
+                        dynamicLabel1.Background = new SolidColorBrush(Colors.White);
+                        dynamicLabel1.BorderThickness = new Thickness(0);
+
+                        border.Child = dynamicLabel1;
+                        Grid.SetRow(border, i);
+                        Grid.SetColumn(border, j);
+                        Grid.Children.Add(border);
                     }
-                    else
-                    {
-                     color = OxyColors.Red;
-
-                     border.BorderBrush = Brushes.DarkRed;
-                    }
-
-                    var series = new LineSeries
-                    {
-                        Color = color,
-                        DataFieldX = "X",
-                        ItemsSource = items
-                    };
-
-                    plotModel.Axes.Add(linearAxis1);
-                    plotModel.Series.Add(series);
-                    plotModel.PlotAreaBorderColor = OxyColors.Transparent;
-                    dynamicLabel1.Model = plotModel;
-
-                    dynamicLabel1.Name = "NewLabel";
-                    dynamicLabel1.Width = 300;
-                    dynamicLabel1.Height = 145;
-                    dynamicLabel1.Foreground = new SolidColorBrush(Colors.Black);
-                    dynamicLabel1.Background = new SolidColorBrush(Colors.White);
-                    dynamicLabel1.BorderThickness = new Thickness(0);
-
-                    border.Child = dynamicLabel1;
-                    Grid.SetRow(border, i);
-                    Grid.SetColumn(border, j);
-                    Grid.Children.Add(border);
                 }
+            }
+            catch (Exception e)
+            {
+                App.Log.Debug(e);
             }
         }
     }
